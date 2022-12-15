b5bcfc0fae23da867a8ae4aa0b773edf1e5fa4c1
Deema AlMutairi <dema77.da@gmail.com>
2022-12-15 13:31:14 +0300

    Authentication

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-
-import LoginScreen from './screens/LoginScreen';
-import SignupScreen from './screens/SignupScreen';
-import WelcomeScreen from './screens/WelcomeScreen';
-import { Colors } from './constants/styles';
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignupScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import { Colors } from "./constants/styles";
+import AuthContextProvider, { AuthContext } from "./store/auth-context";
+import { useContext } from "react";
+import IconButton from "./components/ui/IconButton";
 const Stack = createNativeStackNavigator();
 
 function AuthStack() {
@@ -14,7 +15,7 @@ function AuthStack() {
     <Stack.Navigator
       screenOptions={{
         headerStyle: { backgroundColor: Colors.primary500 },
-        headerTintColor: 'white',
+        headerTintColor: "white",
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
@@ -25,23 +26,48 @@ function AuthStack() {
 }
 
 function AuthenticatedStack() {
+  const authCtx = useContext(AuthContext);
   return (
     <Stack.Navigator
       screenOptions={{
         headerStyle: { backgroundColor: Colors.primary500 },
-        headerTintColor: 'white',
+        headerTintColor: "white",
         contentStyle: { backgroundColor: Colors.primary100 },
+        headerTitleAlign: "center",
+        // headerRight: ({ tintColor }) => (
+        //   <IconButton
+        //     icon="log-out"
+        //     color={tintColor}
+        //     size={24}
+        //     onPress={authCtx.logout}
+        //   />
+        // ),
       }}
     >
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          headerRight: ({ tintColor }) => (
+            <IconButton
+              icon="exit"
+              color={tintColor}
+              size={24}
+              onPress={authCtx.logout}
+            />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 }
 
 function Navigation() {
+  const authCtx = useContext(AuthContext);
   return (
     <NavigationContainer>
-      <AuthStack />
+      {!authCtx.isAuthenticated && <AuthStack />}
+      {authCtx.isAuthenticated && <AuthenticatedStack />}
     </NavigationContainer>
   );
 }
@@ -50,8 +76,9 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-
-      <Navigation />
+      <AuthContextProvider>
+        <Navigation />
+      </AuthContextProvider>
     </>
   );
 }
---
+++ store/auth-context.js
@@ -0,0 +1,45 @@
+import { createContext, useState } from "react";
+
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const AuthContext = createContext({
+  token: "",
+  isAuthenticated: false,
+  email: "",
+  //   emailFunc: (email) => {},
+  authenticate: (token) => {},
+  logout: () => {},
+});
+
+function AuthContextProvider({ children }) {
+  const [authToken, setAuthToken] = useState();
+  const [emailAdd, setEmailAdd] = useState();
+
+  function authenticate(token) {
+    setAuthToken(token);
+    // AsyncStorage.setItem("token", token);
+  }
+
+  function emailFunc(email) {
+    setEmailAdd(email);
+    // AsyncStorage.setItem("email", email);
+  }
+
+  function logout() {
+    setAuthToken(null);
+    // AsyncStorage.removeItem("token");
+  }
+
+  const value = {
+    token: authToken,
+    isAuthenticated: !!authToken,
+    // email: emailAdd,
+    // emailFunc: emailFunc,
+    authenticate: authenticate,
+    logout: logout,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export default AuthContextProvider;
